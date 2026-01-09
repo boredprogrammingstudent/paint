8f1cb645dd30d56f602ba0c218e775de9f113a60
bps <>
2026-01-08 19:27:54 -0500

    removed weird camera system

--- src/drawing_interface.zig
+++ src/drawing_interface.zig
@@ -1,6 +1,5 @@
 const Matrix = @import("matrix.zig");
 const raylib = @import("c.zig").raylib;
-const Camera = @import("camera.zig");
 const BG = @import("bg.zig");
 const ColorPalette = @import("color_palette.zig");
 const Scene = @import("scene.zig");
@@ -8,7 +7,6 @@ const Assets = @import("assets.zig");
 const Window = @import("window.zig");
 
 pub var matrix: Matrix = Matrix.init(16, 16);
-pub var camera: Camera = .{ .width = 1024, .height = 512, .x = 0, .y = 0, .zoom = 10, .speed = 40 };
 pub var bg: BG = .{ .color = raylib.LIGHTGRAY };
 pub var palette: ColorPalette = ColorPalette.default();
 pub var selected_color = raylib.BLACK;
@@ -20,21 +18,17 @@ pub fn init() @This() {
 }
 
 pub fn update(window: *const Window, assets: *Assets) void {
-    camera.checkInput();
-    matrix.checkInput(&camera, &selected_color);
+    matrix.checkInput(window, &selected_color);
     palette.checkInput(window, &selected_color);
     _ = assets;
 }
 
 pub fn draw(window: *const Window, assets: *Assets) void {
     raylib.ClearBackground(bg.color);
-    camera.drawRegion(assets);
-    for (0..matrix.width) |i| {
-        for (0..matrix.height) |j| {
-            matrix.tiles[i][j].draw(&camera);
-        }
-    }
+
+    matrix.draw(window);
     palette.draw(window);
+    _ = assets;
 }
 
 pub fn getMatrix(self: *const @This()) *Matrix {
--- src/matrix.zig
+++ src/matrix.zig
@@ -1,17 +1,17 @@
 const Tile = @import("tile.zig");
-const Camera = @import("camera.zig");
 const raylib = @import("c.zig").raylib;
+const Window = @import("window.zig");
 tiles: [128][128]Tile,
 width: u16,
 height: u16,
-pub fn checkInput(self: *@This(), camera: *Camera, color: *const raylib.Color) void {
+pub fn checkInput(self: *@This(), window: *const Window, color: *const raylib.Color) void {
     if (raylib.IsMouseButtonDown(raylib.MOUSE_BUTTON_LEFT)) {
-        const x: isize = @as(isize, @intFromFloat(raylib.GetMousePosition().x / @as(f32, @floatFromInt(camera.zoom)) + camera.x));
-        const y: isize = @as(isize, @intFromFloat(raylib.GetMousePosition().y / @as(f32, @floatFromInt(camera.zoom)) + camera.y));
-        if (x < 0) return;
-        if (x > self.width) return;
-        if (y < 0) return;
-        if (y > self.height) return;
+        const tileSize = @as(f32, @floatFromInt(window.height)) / @as(f32, @floatFromInt(self.height));
+        const mousePos = raylib.GetMousePosition();
+        const x = @as(isize, @intFromFloat(mousePos.x / tileSize));
+        const y = @as(isize, @intFromFloat(mousePos.y / tileSize));
+        if (x < 0 or x >= self.width) return;
+        if (y < 0 or y >= self.height) return;
         self.tiles[@intCast(x)][@intCast(y)].color = color.*;
     }
 }
@@ -26,4 +26,13 @@ pub fn init(width: u16, height: u16) @This() {
         }
     }
     return matrix;
+}
+
+pub fn draw(self: *@This(), window: *const Window) void {
+    const tileSize = window.height / self.height;
+    for (0..self.width) |i| {
+        for (0..self.height) |j| {
+            raylib.DrawRectangle(@intCast(i * tileSize), @intCast(j * tileSize), tileSize, tileSize, self.tiles[i][j].color);
+        }
+    }
 }
